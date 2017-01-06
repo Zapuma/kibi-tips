ff6d8cfe0ed5845430a9359e0c89d80ea9321eb9
Mihael Liskij <mihael.liskij@gmail.com>
2017-01-06 21:14:48 +0100

    Add splash screen (#1)

--- app/src/main/java/com/zapuma/kibitips/MainMenuActivity.java
+++ app/src/main/java/com/zapuma/kibitips/MainMenuActivity.java
@@ -1,14 +1,19 @@
 package com.zapuma.kibitips;
 
-import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
-public class SplashActivity extends AppCompatActivity {
+public class MainMenuActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        startActivity(new Intent(this, MainMenuActivity.class));
+        setContentView(R.layout.activity_main_menu);
     }
+
+    public void getTipClicked(View view) {
+        System.out.println("bokic");
+    }
+
 }
